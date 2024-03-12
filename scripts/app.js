825ae0059936784436e8f4b0336aeec681a96161
Nathan <antnefedov33@gmail.com>
2024-03-12 16:31:04 +0300

    wrote buttons, wrote styles for them

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
 "use strict"
+
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let box = document.querySelector('.box');
+let degree = 270;
+
+box.addEventListener('click', (e) => {
+    if (e.target.className.includes('prev')) {
+        degree += 90;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg)`;
+    } else if (e.target.className.includes('next')) {
+        degree -= 90;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg)`;
+    }
+})
